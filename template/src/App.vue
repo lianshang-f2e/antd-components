e40967f300b3fe0786d0069f8ac0a674b2118f03
吴茂芳 <maofang.wu@lianshang.com>
2018-10-29 17:47:51 +0800

    fix

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,30 +1,21 @@
 <template>
   <div id="app">
-    <h2>calculate</h2>
-    <sum-function :num1="num1" :num2="num2" v-on:getSumFromChild="receiveChildSum"></sum-function>
-    
-    <p>从子组件获取到的值：{{sumFromChild}}</p>
+    <my-component></my-component>
   </div>
 </template>
 
 <script>
-import sumFunction from '../src/myPlugin/index.js'; // 引入
+import myComponent from '../src/components/index.js'; // 引入
 export default {
   name: 'app',
   data () {
     return {
-      num1: 4,
-      num2: 5,
-      sumFromChild:0,
     }
   },
   components:{ //注册插件
-    sumFunction
+    myComponent
   },
   methods:{
-    receiveChildSum(sum){ //自定义事件，接收子组件的和
-      this.sumFromChild = sum;
-    }
   }
 }
 </script>
--- template/webpack.config.js
+++ template/webpack.config.js
@@ -4,11 +4,11 @@ var webpack = require('webpack')
 const NODE_ENV = process.env.NODE_ENV;
 
 module.exports = {
-  entry: NODE_ENV == 'development' ? './src/main.js' : './src/myPlugin/index.js',
+  entry: NODE_ENV == 'development' ? './src/main.js' : './src/components/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: '{{ name }}.js',
+    filename: 'build.min.js',
     library: '{{ name }}', // 指定的就是你使用require时的模块名
     libraryTarget: 'umd', // 指定输出格式
     umdNamedDefine: true // 会对 UMD 的构建过程中的 AMD 模块进行命名。否则就使用匿名的 define
